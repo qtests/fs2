066b8eea9c405d0a65a3c48e5e74e397e6be6560
qtests <qtests@outlook.com>
2018-07-07 21:34:23 +0200

    Modifying the Filename List at Runtime

--- Foundation.hs
+++ Foundation.hs
@@ -7,18 +7,28 @@
 module Foundation where
 
 import Data.Text
+import Control.Concurrent.STM
 
 import Yesod
 
 -- data App = App
-data App = App [Text]
+-- data App = App [Text]
+data App = App (TVar [Text])
+
 instance Yesod App
 
 mkYesodData "App" $(parseRoutesFile "config/routes")
 
 getList :: Handler [Text]
 getList = do
-    App state <- getYesod
-    return state
-    
+--     App state <- getYesod
+--     return state
+   App tstate <- getYesod
+   liftIO $ readTVarIO tstate
+
+addFile :: App -> Text -> Handler ()
+addFile (App tstore) op =
+    liftIO . atomically $ do
+    modifyTVar tstore $ \ ops -> op : ops
+
 -- /show
--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,17 @@
 module Main where
 
 import Yesod
-  
+import Control.Concurrent.STM
+
 import Dispatch ()
 import Foundation
   
 -- http://localhost:3000/
 main :: IO ()
 -- main = warp 3000 App
-main = warp 3000 $ App ["readme.txt", "report.pdf", "music.wav"]
+-- main = warp 3000 $ App ["readme.txt", "report.pdf", "music.wav"]
+main = do
+    tfilenames <- atomically $ newTVar ["readme.txt", "report.pdf", "music.wav"]
+    warp 3000 $ App tfilenames
+    
 
